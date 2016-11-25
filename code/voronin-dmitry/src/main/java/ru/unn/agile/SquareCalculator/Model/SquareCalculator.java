b5452435363dde387c99b2ae36d1423d02a093fb
dima <dimadimadima051294@gmail.com>
2016-11-25 04:00:45 +0300

    Add new files

--- code/voronin-dmitry/src/main/java/ru/unn/agile/SquareCalculator/Model/SquareCalculator.java
+++ code/voronin-dmitry/src/main/java/ru/unn/agile/SquareCalculator/Model/SquareCalculator.java
@@ -8,7 +8,7 @@ public class SquareCalculator {
 
         checkParams("Radius must not be negative!", radius);
 
-        return 4* Math.PI*radius*radius;
+        return SquareCalculatorConstants.SphereConst* Math.PI*radius*radius;
 
     }
 
@@ -19,7 +19,17 @@ public class SquareCalculator {
 
     public double getCubeSquare(double lenght) {
         checkParams("Lenght must not be negative!" , lenght);
-        return 6*lenght*lenght;
+        return SquareCalculatorConstants.CubeConst*lenght*lenght;
+    }
+
+    public double getCylinderSquare(double radius , double height) {
+        checkParams("Radius and height must not be negative!", radius , height);
+        return SquareCalculatorConstants.CylinderConst*Math.PI*radius*(radius+height);
+    }
+
+    public double getParallelepipedSquare(double height, double lenght, double width) {
+        checkParams("height , lenght and width must not be negative!",height,lenght,width);
+        return SquareCalculatorConstants.ParallelepipedConst*(height*lenght+height*width+lenght*width);
     }
 
     private void checkParams(final String message, final double... params) {
@@ -28,17 +38,6 @@ public class SquareCalculator {
                 throw new IllegalArgumentException(message);
             }
         }
-    }
-
-
-    public double getCylinderSquare(double radius , double height) {
-        checkParams("Radius and height must not be negative!", radius , height);
-        return 2*Math.PI*radius*(radius+height);
-    }
-
-    public double getParallelepipedSquare(double height, double lenght, double width) {
-        checkParams("height , lenght and width must not be negative!",height,lenght,width);
-        return 2*(height*lenght+height*width+lenght*width);
     }
 }
 
---
+++ code/voronin-dmitry/src/main/java/ru/unn/agile/SquareCalculator/Model/SquareCalculatorConstants.java
@@ -0,0 +1,12 @@
+package ru.unn.agile.SquareCalculator.Model;
+
+/**
+ * Created by Дмитрий on 25.11.2016.
+ */
+public class SquareCalculatorConstants {
+
+    public static final  int SphereConst = 4;
+    public  static final int CubeConst = 6;
+    public static final int CylinderConst = 2;
+    public static final int ParallelepipedConst = 2;
+}
