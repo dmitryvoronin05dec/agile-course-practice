5fdd6117a92e482309848d132050b648805fd171
dima <dimadimadima051294@gmail.com>
2016-12-20 01:45:11 +0300

    add files

---
+++ code/voronin-dmitry/View/src/main/java/ru/unn/agile/Huffman/Main.java
@@ -0,0 +1,26 @@
+package ru.unn.agile.Huffman;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Huffman.FXML"));
+        primaryStage.setTitle("Операции над трёхмерными векторами");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        double height = primaryStage.getHeight();
+        primaryStage.setMinHeight(height);
+        primaryStage.setMaxHeight(height);
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}
